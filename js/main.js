58963832f72a82a2b1e7b0d14df2fd35d40730b7
Whit Jackson <whit@whitjack.me>
2015-11-17 11:02:39 -0800

    New

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-$(".bitcoin-address").text("19356KxTs9Bw5AAdxens5hoxDSp5bsUKse")
-bitcoinaddress.init({
-            selector: ".bitcoin-address",
-            template: "bitcoin-address-template",
-            qr : {
-                width: 256,
-                height: 256,
-                colorDark : "#000000",
-                colorLight : "#ffffff"
-            },
-            qrRawAddress: false
+$.get("http://sleepy-caverns-2145.herokuapp.com/address", function(data) {
+    $(".bitcoin-address").attr("data-bc-address", data) 
+    bitcoinaddress.init({
+        selector: ".bitcoin-address",
+        template: "bitcoin-address-template",
+        qr : {
+            width: 256,
+            height: 256,
+            colorDark : "#000000",
+            colorLight : "#ffffff"
+        },
+        qrRawAddress: false
+    });
 });
